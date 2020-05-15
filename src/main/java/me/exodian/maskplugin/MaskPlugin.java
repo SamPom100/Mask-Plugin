6b80a10a288549c0c53d7a9844007bb3843297eb
Joseph Zurowski <Dunklheit@users.noreply.github.com>
2020-05-14 20:11:27 -0400

    Remove unused import

--- src/main/java/me/exodian/maskplugin/MaskPlugin.java
+++ src/main/java/me/exodian/maskplugin/MaskPlugin.java
@@ -12,9 +12,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-
-import org.bukkit.event.player.PlayerInteractEvent;
-
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
